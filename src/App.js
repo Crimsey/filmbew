e2e02521aee0049b26fa8f48749e177c6a22a373
Crimsey <kubsok2@wp.pl>
2021-12-29 20:07:46 +0100

    przechodzenie między stronami

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+//import Home from "./Home";
+import Contact from "./Contact";
+//import About from "./About";
+//import Header from "./Header";
+import { Route, Switch } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
 
 
 function App() {
   return (
     <div className="App">
+      <Switch>
+        <Route exact path="/contact" render={props => <Contact {...props} />} />
+      </Switch>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
